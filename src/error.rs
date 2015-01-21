8617f4958c4c9da2691cc9efba78fe1f86d24cd5
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-01-21 14:36:26 +0100

    Move the macros from error to support

--- src/error.rs
+++ src/error.rs
@@ -1,22 +1,7 @@
-#![macro_use]
-
 #[derive(Show)]
 pub struct Error {
     message: String,
 }
-
-macro_rules! raise(
-    ($message:expr) => (return Err(Error::new($message)));
-);
-
-macro_rules! some(
-    ($option:expr, $message:expr) => (
-        match $option {
-            Some(object) => object,
-            None => raise!($message),
-        }
-    );
-);
 
 impl Error {
     #[inline]
--- src/support.rs
+++ src/support.rs
@@ -3,3 +3,16 @@
 macro_rules! is_nil(
     ($result:expr) => ($result == ::cocoa::base::nil);
 );
+
+macro_rules! raise(
+    ($message:expr) => (return Err(Error::new($message)));
+);
+
+macro_rules! some(
+    ($option:expr, $message:expr) => (
+        match $option {
+            Some(object) => object,
+            None => raise!($message),
+        }
+    );
+);
