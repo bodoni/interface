d2d0cf0e7e14f991efa57e2df25009b40749f45b
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-01-01 13:15:42 +0100

    Start to construct glyph outlines

--- src/lib/glyph.rs
+++ src/lib/glyph.rs
@@ -37,8 +37,74 @@ impl Object for Glyph {
 }
 
 fn construct<'l>(mut program: Program<'l>) -> Result<Vec<Point>> {
+    use opentype::postscript::type2::Operator::*;
+
     let vertices = Vec::new();
-    while let Some((_, _)) = ok!(program.next()) {
+    while let Some((operator, operands)) = ok!(program.next()) {
+        let count = operands.len();
+        match operator {
+            RMoveTo => {
+                assert!(count == 2);
+            },
+            HMoveTo => {
+                assert!(count == 1);
+            },
+            VMoveTo => {
+                assert!(count == 1);
+            },
+            RLineTo => {
+                assert!(count % 2 == 0);
+            },
+            HLineTo => {
+                if count % 2 == 0 {
+                } else {
+                }
+            },
+            VLineTo => {
+                if count % 2 == 0 {
+                } else {
+                }
+            },
+            RRCurveTo => {
+                assert!(count % 6 == 0);
+            },
+            HHCurveTo => {
+                if count % 4 == 0 {
+                } else {
+                    assert!((count - 1) % 4 == 0);
+                }
+            },
+            HVCurveTo => {
+                if count % 8 == 0 {
+                } else if (count - 1) % 8 == 0 {
+                } else if (count - 4) % 8 == 0 {
+                } else {
+                    assert!((count - 4 - 1) % 8 == 0);
+                }
+            },
+            VHCurveTo => {
+                if count % 8 == 0 {
+                } else if (count - 1) % 8 == 0 {
+                } else if (count - 4) % 8 == 0 {
+                } else {
+                    assert!((count - 4 - 1) % 8 == 0);
+                }
+            },
+            VVCurveTo => {
+                if count % 4 == 0 {
+                } else {
+                    assert!((count - 1) % 4 == 0);
+                }
+            },
+            RCurveLine => {
+                assert!(count >= 2 && (count - 2) % 6 == 0);
+            },
+            RLineCurve => {
+                assert!(count >= 6 && (count - 6) % 2 == 0);
+            },
+            HStem | VStem | HStemHM | VStemHM | HintMask | CntrMask => {},
+            _ => unreachable!(),
+        }
     }
     Ok(vertices)
 }
